443ae6c3eafb5a4767aefa6b0f6a68c2aa4999cc
Catalin Morosan <catalin.morosan@gmail.com>
2022-12-31 09:32:32 +0200

    Language support

--- app/__brick__/{{ projectName.lowerCase() }}/lib/app/app_strings.dart
+++ app/__brick__/{{ projectName.lowerCase() }}/lib/app/app_strings.dart
@@ -2,10 +2,20 @@ import 'package:auto_localized/annotations.dart';
 
 @AutoLocalized(
   locales: <AutoLocalizedLocale>[
-    AutoLocalizedLocale(
+    {{#useEn}}AutoLocalizedLocale(
       languageCode: 'en',
-      translationsFiles: ['resources/lang/en.yaml'],
-    ),
+      translationsFiles: [
+        'resources/lang/en.yaml',
+        'resources/lang/en_generic.yaml',
+      ],
+    ),{{/useEn}}
+    {{#useRo}}AutoLocalizedLocale(
+      languageCode: 'ro',
+      translationsFiles: [
+        'resources/lang/ro.yaml',
+        'resources/lang/ro_generic.yaml',
+      ],
+    ),{{/useRo}}
   ],
 )
 class $Strings {}
