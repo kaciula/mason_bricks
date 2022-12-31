9a7fc4cba1ef77ec035b739bc9a7ddfa881361b0
Catalin Morosan <catalin.morosan@gmail.com>
2022-12-31 08:05:29 +0200

    Misc

--- app/__brick__/{{ projectName.lowerCase() }}/lib/start/service_locator.dart
+++ app/__brick__/{{ projectName.lowerCase() }}/lib/start/service_locator.dart
@@ -4,7 +4,7 @@ import 'package:{{ projectName.snakeCase() }}/app/app_messenger.dart';
 import 'package:{{ projectName.snakeCase() }}/app/app_navigator.dart';
 import 'package:{{ projectName.snakeCase() }}/app/cubit/app_cubit.dart';
 import 'package:{{ projectName.snakeCase() }}/features/common/data/local/app_info_store.dart';
-import 'package:{{ projectName.snakeCase() }}/features/common/data/remote/remote_data_store.dart';
+{{#useDio}}import 'package:{{ projectName.snakeCase() }}/features/common/data/remote/remote_data_store.dart';{{/useDio}}
 import 'package:{{ projectName.snakeCase() }}/services/custom/firebase/fire_service.dart';
 import 'package:{{ projectName.snakeCase() }}/services/generic/crash/crash_service.dart';
 import 'package:{{ projectName.snakeCase() }}/services/generic/file_storage_service.dart';
