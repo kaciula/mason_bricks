9e7c1fc902e18a447e7f4c10d5733d90e0405430
Catalin Morosan <catalin.morosan@gmail.com>
2024-04-05 11:48:22 +0300

    Updates to FileStorageService

--- app/__brick__/{{ projectName.lowerCase() }}/lib/services/generic/file_storage_service.dart
+++ app/__brick__/{{ projectName.lowerCase() }}/lib/services/generic/file_storage_service.dart
@@ -2,6 +2,7 @@ import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:universal_io/io.dart';
+import 'package:uuid/uuid.dart';
 
 class FileStorageService {
   late Directory cacheDir;
@@ -11,6 +12,9 @@ class FileStorageService {
 
   Future<void> init() async {
     cacheDir = await getTemporaryDirectory();
+    // Create inner cache directory so you can delete all files in it without affecting the caches of other plugins such as Crashlytics on iOS
+    cacheDir =
+        await Directory('${cacheDir.path}/inner').create(recursive: true);
     appSupportDir = await getApplicationSupportDirectory();
     appDataDir = await getApplicationDocumentsDirectory();
     await _initDownloadsDir();
@@ -37,12 +41,32 @@ class FileStorageService {
     return File('${cacheDir.path}/$fileName');
   }
 
+  File randomCacheFile({required String extension}) {
+    return cacheFile('${Uuid().v1()}.$extension');
+  }
+
+  Directory cacheDirectory(String dirName) {
+    return Directory('${cacheDir.path}/$dirName');
+  }
+
+  Directory randomCacheDirectory() {
+    return cacheDirectory(Uuid().v1());
+  }
+
   File appSupportFile(String fileName) {
     return File('${appSupportDir.path}/$fileName');
   }
 
   File appDataFile(String fileName) {
     return File('${appDataDir.path}/$fileName');
+  }
+
+  File randomAppDataFile({required String extension}) {
+    return appDataFile('${Uuid().v1()}.$extension');
+  }
+
+  Directory appDataDirectory(String fileName) {
+    return Directory('${appDataDir.path}/$fileName');
   }
 
   File downloadsFile(String fileName) {
@@ -70,11 +94,11 @@ class FileStorageService {
     Uint8List bytes, {
     required String fileName,
     required FileLocation location,
-    bool override = false,
+    bool override = true,
   }) async {
     final File file = _file(fileName, location);
     // ignore: avoid_slow_async_io
-    if (!await file.exists() || override) {
+    if (override || !await file.exists()) {
       return await file.writeAsBytes(bytes, flush: true);
     } else {
       return file;
@@ -105,12 +129,22 @@ class FileStorageService {
     }
   }
 
-  void deleteFile(String path) {
-    File(path).deleteSync();
+  Future<void> deleteFile(String path) async {
+    await File(path).delete();
   }
 
-  void deleteDirectory(String path) {
-    File(path).deleteSync(recursive: true);
+  Future<void> deleteDirectory(String path) async {
+    await File(path).delete(recursive: true);
+  }
+
+  Future<void> clearCache() async {
+    cacheDir.list().forEach((it) async {
+      if (it is File) {
+        await it.delete();
+      } else if (it is Directory) {
+        await it.delete(recursive: true);
+      }
+    });
   }
 }
 
